052a7256f5a827232f0bead6b33271291d1fa61c
Matojeje <matojeje@gmail.com>
2019-06-13 07:22:59 +0200

    OK

---
+++ jsxm/xmlist.js
@@ -0,0 +1,6 @@
+baseuri = "../mus/";
+xmuris = [
+	["SuperTux - mortimers chipdisko", "mortimers_chipdisko_fixed.xm"],
+	["Wii Shop Channel", 'wiiShopCh.xm'],
+	["Lizardking - Claustrophobia", "Claustrophobia.xm"]
+];
